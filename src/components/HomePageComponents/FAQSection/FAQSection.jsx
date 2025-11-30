3265e8bc40cd3880cc81c03b370e1f37aec439c9
ArunRoy404 <arunroy0065@gmail.com>
2025-11-30 11:38:05 +0600

    feat: FAQ section created

--- src/components/HomePageComponents/FAQSection/FAQSection.jsx
+++ src/components/HomePageComponents/FAQSection/FAQSection.jsx
@@ -17,14 +17,14 @@ const FAQSection = () => {
 
             {/* faq accordion  */}
             <Accordion type="single" collapsible defaultValue={1}>
-                <div className="grid grid-cols-2 gap-10">
-                    <div className="justify-between flex flex-col item-start">
+                <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 lg:gap-10">
+                    <div className="justify-between flex flex-col item-start gap-4 lg:gap-7">
                         {faqData?.map((faq, idx) => {
                             if (idx < faqData.length / 2) {
                                 return (
                                     <AccordionItem key={idx} value={idx + 1}>
                                         <AccordionTrigger>
-                                            {faq?.id}. {faq?.qs}
+                                            {faq?.qs}
                                         </AccordionTrigger>
                                         <AccordionContent>{faq?.ans}</AccordionContent>
                                     </AccordionItem>
@@ -32,13 +32,13 @@ const FAQSection = () => {
                             }
                         })}
                     </div>
-                    <div className="justify-between flex flex-col item-start">
+                    <div className="justify-between flex flex-col item-start gap-4 lg:gap-7">
                         {faqData?.map((faq, idx) => {
                             if (idx >= faqData.length / 2) {
                                 return (
                                     <AccordionItem key={idx} value={idx + 1}>
                                         <AccordionTrigger>
-                                            {faq?.id}. {faq?.qs}
+                                            {faq?.qs}
                                         </AccordionTrigger>
                                         <AccordionContent>{faq?.ans}</AccordionContent>
                                     </AccordionItem>
--- src/components/ui/accordion.jsx
+++ src/components/ui/accordion.jsx
@@ -2,63 +2,91 @@
 
 import * as React from "react"
 import * as AccordionPrimitive from "@radix-ui/react-accordion"
-import { ChevronDownIcon } from "lucide-react"
-
+import { ChevronDownIcon, Minus, Plus } from "lucide-react"
 import { cn } from "@/lib/utils"
 
-function Accordion({
-  ...props
-}) {
-  return <AccordionPrimitive.Root data-slot="accordion" {...props} />;
+function Accordion(props) {
+  return <AccordionPrimitive.Root data-slot="accordion" {...props} />
 }
 
-function AccordionItem({
-  className,
-  ...props
-}) {
+function AccordionItem({ className, ...props }) {
   return (
     <AccordionPrimitive.Item
       data-slot="accordion-item"
-      className={cn("border-b last:border-b-0", className)}
-      {...props} />
-  );
+      className={cn(
+        "border border-[rgba(75,85,99,0.25)] px-10 py-9 rounded-2xl",
+        "data-[state=open]:shadow-[0_24.556px_32.742px_-14.734px_rgba(149,149,149,0.25)]",
+
+        /* responsive only for md and below */
+        "md:px-6 md:py-6 px-4 py-5",
+
+        className
+      )}
+      {...props}
+    />
+  )
 }
 
-function AccordionTrigger({
-  className,
-  children,
-  ...props
-}) {
+function AccordionTrigger({ className, children, ...props }) {
   return (
-    <AccordionPrimitive.Header className="flex">
+    <AccordionPrimitive.Header className="flex w-full">
       <AccordionPrimitive.Trigger
         data-slot="accordion-trigger"
         className={cn(
-          "focus-visible:border-ring focus-visible:ring-ring/50 flex flex-1 items-start justify-between gap-4 rounded-md py-4 text-left text-sm font-medium transition-all outline-none hover:underline focus-visible:ring-[3px] disabled:pointer-events-none disabled:opacity-50 [&[data-state=open]>svg]:rotate-180",
+          "focus-visible:border-ring focus-visible:ring-ring/50 flex flex-1 items-start gap-4 rounded-md text-left text-sm font-medium transition-all outline-none focus-visible:ring-[3px] disabled:pointer-events-none disabled:opacity-50",
+          "group hover:underline-0 gap-14 cursor-pointer text-primary-foreground text-lg font-semibold",
+
+          /* responsive only */
+          "gap-8 md:text-base text-sm",
+
           className
         )}
-        {...props}>
+        {...props}
+      >
+        <div className="relative">
+          <Plus
+            className="
+              absolute accordion-icon1 text-black pointer-events-none lg:size-6 translate-y-0.5 transition-transform duration-200
+              md:size-5 size-4
+            "
+          />
+          <Minus
+            className="
+              absolute accordion-icon2 text-primary pointer-events-none lg:size-6 translate-y-0.5 transition-transform duration-200
+              md:size-5 size-4
+            "
+          />
+        </div>
+
         {children}
-        <ChevronDownIcon
-          className="text-muted-foreground pointer-events-none size-4 shrink-0 translate-y-0.5 transition-transform duration-200" />
       </AccordionPrimitive.Trigger>
     </AccordionPrimitive.Header>
-  );
+  )
 }
 
-function AccordionContent({
-  className,
-  children,
-  ...props
-}) {
+function AccordionContent({ className, children, ...props }) {
   return (
     <AccordionPrimitive.Content
       data-slot="accordion-content"
       className="data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down overflow-hidden text-sm"
-      {...props}>
-      <div className={cn("pt-0 pb-4", className)}>{children}</div>
+      {...props}
+    >
+      <div
+        className={cn(
+          "text-faq-color text-sm pt-7 flex gap-14",
+
+          /* responsive */
+          "md:pt-5 md:gap-8 md:text-[13px] pt-4 gap-4 text-[12px]",
+
+          className
+        )}
+      >
+        <div className="hidden md:block"></div>
+
+        {children}
+      </div>
     </AccordionPrimitive.Content>
-  );
+  )
 }
 
 export { Accordion, AccordionItem, AccordionTrigger, AccordionContent }
