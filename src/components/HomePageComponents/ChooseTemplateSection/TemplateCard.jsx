7273feba58b41f161c99ec3aa506f9e1114d836a
Arun Roy <arunroy0065@gmail.com>
2025-12-10 13:10:52 +0600

    fix: some issue fixed

--- src/components/HomePageComponents/ChooseTemplateSection/TemplateCard.jsx
+++ src/components/HomePageComponents/ChooseTemplateSection/TemplateCard.jsx
@@ -3,13 +3,13 @@ import TemplateCardClick from './TemplateCardClick';
 
 const TemplateCard = ({ template }) => {
     return (
-        <div className='relative cursor-pointer flex flex-col items-center gap-4 md:gap-8'>
+        <div className='group relative cursor-pointer flex flex-col items-center gap-4 md:gap-8'>
             <TemplateCardClick template={template} />
             <div className='aspect-3/4 w-full rounded-3xl overflow-hidden'>
                 <Image
                     src={template?.src}
                     alt={template?.title || 'Template image'}
-                    className="object-cover hover:scale-105 transition-ease-in-out w-full h-full"
+                    className="object-cover group-hover:scale-105 transition-ease-in-out w-full h-full"
                     loading="eager"
                 />
             </div>
