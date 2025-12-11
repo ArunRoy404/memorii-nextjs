8ee34a59f721ef074774ebba03d84df9ecedddba
Arun Roy <arunroy0065@gmail.com>
2025-12-11 18:15:59 +0600

    feat: some issue fixed

--- src/components/EditorComponents/TextOptions/TextOptions.jsx
+++ src/components/EditorComponents/TextOptions/TextOptions.jsx
@@ -1,26 +1,9 @@
 import { DropdownMenuContent } from "../../ui/dropdown-menu";
-import {
-    BoldIcon,
-    ItalicIcon,
-    UnderlineIcon,
-    StrikethroughIcon,
-    TextAlignCenter,
-    List,
-} from "lucide-react";
 import FontStyleSelection from "./FontStyleSelection";
-import { Button } from "@/components/ui/button";
 import FontSizeSelection from "./FontSizeSelection";
 import TextColorPicker from "./TextColorPicker";
-
-
-const ICONS = [
-    BoldIcon,
-    ItalicIcon,
-    UnderlineIcon,
-    StrikethroughIcon,
-    TextAlignCenter,
-    List,
-];
+import TextStyles from "./TextStyles";
+import TextAlignment from "./TextAlignment";
 
 const TextOptions = () => {
     return (
@@ -33,19 +16,9 @@ const TextOptions = () => {
             <FontSizeSelection />
             <TextColorPicker />
 
-
-
-            <div className="flex flex-col items-center gap-1">
-                {ICONS.map((Icon) => (
-                    <Button
-                        key={Icon.displayName ?? Icon.name}
-                        variant="ghost"
-                        size="icon"
-                        className="mx-auto flex h-7 w-7 flex-col items-center justify-center p-0"
-                    >
-                        <Icon size={15} />
-                    </Button>
-                ))}
+            <div className="flex flex-col items-center gap-1 w-full">
+                <TextStyles />
+                <TextAlignment />
             </div>
         </DropdownMenuContent>
     );
