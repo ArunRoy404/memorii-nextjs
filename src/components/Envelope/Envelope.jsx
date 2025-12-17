99f58952b86ea6eaef8b6279d5f2b2599b98c167
Arun Roy <arunroy0065@gmail.com>
2025-12-17 10:14:41 +0600

    feat: animation slightly works

--- src/components/Envelope/Envelope.jsx
+++ src/components/Envelope/Envelope.jsx
@@ -18,33 +18,49 @@ import gsap from 'gsap';
 const Envelope = ({ className }) => {
     const [showText, setShowText] = useState(false)
     const [text, setText] = useState('')
-    const envelopRef = useRef(null)
-    const envelopCoverRef = useRef(null)
+    const envelopeContainerRef = useRef(null)
+    const envelopeCoverRef1 = useRef(null)
+    const envelopeCoverRef2 = useRef(null)
+    const envelope = useRef(null)
 
 
     useEffect(() => {
-        gsap.set(envelopCoverRef.current, {
+        gsap.set(envelopeCoverRef1.current, {
             transformOrigin: 'top center',
         });
-        gsap.set(envelopRef.current, {
+        gsap.set(envelopeCoverRef2.current, {
+            transformOrigin: 'top center',
+        });
+        gsap.set(envelopeContainerRef.current, {
             transformOrigin: 'top center',
         });
     }, [])
 
     const handleOpen = () => {
-        gsap.to(envelopCoverRef.current, {
+        gsap.to(envelopeCoverRef1.current, {
             rotateX: 180,
-
+            duration: 1.2,
+            ease: 'power3.inOut',
+        });
+        gsap.to(envelopeCoverRef2.current, {
+            rotateX: 180,
             duration: 1.2,
             ease: 'power3.inOut',
             onComplete: () => {
-                gsap.set(envelopCoverRef.current, { zIndex: 0 });
+                gsap.set(envelopeCoverRef2.current, { opacity: 0 });
             }
         });
 
 
-        gsap.to(envelopRef.current, {
+        gsap.to(envelopeContainerRef.current, {
             translateY: '100',
+            duration: 1.2,
+            ease: 'power3.inOut',
+        })
+
+        gsap.to(envelope.current, {
+            translateY: '500%',
+            delay: 1.3,
             duration: 1.2,
             ease: 'power3.inOut',
         })
@@ -54,111 +70,152 @@ const Envelope = ({ className }) => {
 
     return (
         <div
-            ref={envelopRef}
+            ref={envelopeContainerRef}
             className='relative max-w-max max-h-max '>
 
+
+            {/* content  */}
+            {/* <div
+                className='absolute overflow-hidden h-full w-full -top-[100px]'
+            > */}
             {/* content */}
-            <Image
-                src={anniversaryTemplate}
-                alt='envelope content'
-                className='absolute left-[50%] -translate-x-[50%] z-1 w-[90%] '
-            />
-
-            <div className=''/>
-
-
-
-            {/* cover  */}
-            <div
-                ref={envelopCoverRef}
-                className='max-h-max w-full absolute top-0 z-3'
-            >
-                <Image
-                    src={EnvelopeCover}
-                    alt='envelope cover'
-                    className='w-full'
+            {/* <Image
+                    src={anniversaryTemplate}
+                    alt='envelope content'
+                    className='absolute left-[50%] -translate-x-[50%] w-[900px] h-auto z-1'
                 />
-            </div>
+            </div> */}
 
 
 
+
+
+
+
+            {/* envelope  */}
             <div
+                ref={envelope}
                 onClick={handleOpen}
                 className={cn(
-                    'cursor-pointer w-[1000px] aspect-[1.76]! bg-envelope relative z-2 flex flex-col',
+                    'cursor-pointer w-[1000px] aspect-[1.76]! bg-envelope relative flex flex-col z-2',
                     className
                 )}
             >
-                {/* wave  */}
+
+                {/* cover  */}
                 <div
-                    className='absolute top-0 max-h-max w-full'
+                    ref={envelopeCoverRef1}
+                    className='max-h-max w-full absolute top-0 z-0'
                 >
                     <Image
-                        src={EnvelopeDecorationWaves}
-                        alt='envelope decoration waves'
+                        src={EnvelopeCover}
+                        alt='envelope cover'
                         className='w-full'
                     />
                 </div>
 
 
-                {/* content  */}
+                {/* content */}
                 <div
-                    className='relative w-full flex-1'
+                    className='absolute overflow-hidden -top-[100px] h-full w-full'
                 >
                     <Image
-                        src={EnvelopeLeafBottom}
+                        src={anniversaryTemplate}
                         alt='envelope content'
-                        className='absolute bottom-0 left-0'
+                        className='absolute left-[50%] -top-[100px] -translate-x-[50%] w-[900px] h-auto z-1'
                     />
-                    <Image
-                        src={EnvelopeLeafTop}
-                        alt='envelope content'
-                        className='absolute top-0 right-0'
-                    />
-                    <Image
-                        src={EnvelopeStamp}
-                        alt='envelope content'
-                        className='absolute bottom-4 right-4'
-                    />
+                </div>
 
+
+
+                {/* envelope upper content  */}
+                <div
+                    className='bg-envelope relative z-10 h-full '
+                >
 
                     <div
-                        onClick={() => console.log('div clicked')}
-                        className='pl-20 text-envelope-text pt-30 relative z-2'
+                        ref={envelopeCoverRef2}
+                        className='max-h-max w-full absolute top-0 z-3'
                     >
-                        {
-                            showText ?
-                                (<div>
-                                    <h2 className='text-3xl'>To: </h2>
-                                    <input
-                                        type='text'
-                                        className={`text-[40px] w-80 ${!text ? 'border border-dashed' : ''}`}
-                                        value={text}
-                                        onChange={(e) => setText(e.target.value)}
-                                    />
-                                </div>
-                                ) : (
-                                    <div
-                                        onClick={() => setShowText(true)}
-                                        className='cursor-pointer flex items-center gap-2 border w-80 p-4 border-dashed'
-                                    >
-                                        <span className='border '>
-                                            <Type size={14} />
-                                        </span>
-                                        <span>Add Text</span>
-                                    </div>
-                                )
-                        }
+                        <Image
+                            src={EnvelopeCover}
+                            alt='envelope cover'
+                            className='w-full'
+                        />
                     </div>
 
 
-
-                    <Button
-                        variant='link'
-                        className='absolute bottom-10 left-20 p-0  underline hover:scale-100! active:scale-100! text-envelope-text'
+                    {/* wave  */}
+                    <div
+                        className='absolute top-0 max-h-max w-full'
                     >
-                        Generate the link
-                    </Button>
+                        <Image
+                            src={EnvelopeDecorationWaves}
+                            alt='envelope decoration waves'
+                            className='w-full'
+                        />
+                    </div>
+
+
+                    {/* decorations  */}
+                    <div
+                        className='relative w-full h-full flex-1'
+                    >
+                        <Image
+                            src={EnvelopeLeafBottom}
+                            alt='envelope content'
+                            className='absolute bottom-0 left-0'
+                        />
+                        <Image
+                            src={EnvelopeLeafTop}
+                            alt='envelope content'
+                            className='absolute top-0 right-0'
+                        />
+                        <Image
+                            src={EnvelopeStamp}
+                            alt='envelope content'
+                            className='absolute bottom-4 right-4'
+                        />
+
+
+                        <div
+                            onClick={() => console.log('div clicked')}
+                            className='pl-20 text-envelope-text pt-80 relative z-2'
+                        >
+                            {
+                                showText ?
+                                    (<div>
+                                        <h2 className='text-3xl'>To: </h2>
+                                        <input
+                                            type='text'
+                                            className={`text-[40px] w-80 ${!text ? 'border border-dashed' : ''}`}
+                                            value={text}
+                                            onChange={(e) => setText(e.target.value)}
+                                        />
+                                    </div>
+                                    ) : (
+                                        <div
+                                            onClick={() => setShowText(true)}
+                                            className='cursor-pointer flex items-center gap-2 border w-80 p-4 border-dashed'
+                                        >
+                                            <span className='border '>
+                                                <Type size={14} />
+                                            </span>
+                                            <span>Add Text</span>
+                                        </div>
+                                    )
+                            }
+                        </div>
+
+
+
+                        <Button
+                            variant='link'
+                            className='absolute bottom-10 left-20 p-0  underline hover:scale-100! active:scale-100! text-envelope-text'
+                        >
+                            Generate the link
+                        </Button>
+                    </div>
                 </div>
             </div>
         </div>
