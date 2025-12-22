0164c1c8d51a8c1d8c29d19bd6c31eb12a737555
Arun Roy <arunroy0065@gmail.com>
2025-12-22 17:57:26 +0600

    refactor: add page option added into editor card

--- src/pages/editor/CardEditorPage.jsx
+++ src/pages/editor/CardEditorPage.jsx
@@ -2,13 +2,15 @@
 
 import CardOptions from '@/components/EditorComponents/CardOptions';
 import CardEditor from '@/components/EditorComponents/Editor/CardEditor';
-import CardTextInsert from '@/components/EditorComponents/Editor/CardTextInsert';
+import { useEditorStore } from '@/store/useEditorStore';
 import { useEditorTemplateStore } from '@/store/useEditorTemplateStore';
+import { Plus } from 'lucide-react';
 import Image from 'next/image';
 import React from 'react';
 
 const CardEditorPage = () => {
     const { selectedTemplate } = useEditorTemplateStore();
+    const { addPage } = useEditorStore();
 
     let width = selectedTemplate?.src?.width;
     let height = selectedTemplate?.src?.height;
@@ -21,42 +23,55 @@ const CardEditorPage = () => {
             className='container mx-auto px-4 lg:px-0 h-full flex items-center justify-center'
         >
             <div
-                className='flex gap-10 mx-auto'
+                className='space-y-2'
             >
-                {/* template front  */}
-                <div className={`relative max-w-[500px]`}
-                    style={{ width, aspectRatio }}
+                <div
+                    className='flex gap-10 mx-auto'
                 >
-                    {
-                        !!selectedTemplate && (
-                            <Image
-                                src={selectedTemplate?.src}
-                                alt={selectedTemplate?.title || 'Template image'}
-                                fill
-                            // className="object-cover"
-                            />
-                        )
-                    }
+                    {/* template front  */}
+                    <div className={`relative max-w-[500px]`}
+                        style={{ width, aspectRatio }}
+                    >
+                        {
+                            !!selectedTemplate && (
+                                <Image
+                                    src={selectedTemplate?.src}
+                                    alt={selectedTemplate?.title || 'Template image'}
+                                    fill
+                                // className="object-cover"
+                                />
+                            )
+                        }
+                    </div>
+
+
+                    {/* options  */}
+                    <CardOptions />
+
+
+
+                    {/* page and text options  */}
+                    <div
+                        className='flex max-h-max'
+                    >
+                        {/* editor  */}
+                        {/* <div
+                        className='w-1/2 aspect-3/4'
+                    ></div> */}
+                        <CardEditor />
+
+                        {/* add text options  */}
+                        {/* <CardTextInsert /> */}
+                    </div>
                 </div>
 
 
-                {/* options  */}
-                <CardOptions />
-
-
-
-                {/* page and text options  */}
                 <div
-                    className='flex max-h-max'
+                    onClick={() => addPage()}
+                    className='flex items-center gap-2 w-full justify-center border-2 rounded-full py-2 cursor-pointer'
                 >
-                    {/* editor  */}
-                    {/* <div
-                        className='w-1/2 aspect-3/4'
-                    ></div> */}
-                    <CardEditor />
-
-                    {/* add text options  */}
-                    {/* <CardTextInsert /> */}
+                    <Plus />
+                    <p>Add Page</p>
                 </div>
             </div>
         </div>
