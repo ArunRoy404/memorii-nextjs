d77f5cae0abe366557f45032f87f04407bb25a85
Arun Roy <arunroy0065@gmail.com>
2025-12-23 17:37:05 +0600

    feat: drafts page added in the dashboard

---
+++ src/pages/dashboard/DraftsPage.jsx
@@ -0,0 +1,30 @@
+import draftImage from '@/assets/images/draftImage.jpg'
+import DashboardCard from '@/components/dashboardComponents/DashboardCard';
+
+
+function DraftsPage() {
+    const cards = Array(9).fill({
+        title: 'Birthday Wishes for Mom',
+        type: 'e-card',
+        lastEdited: 'Last edited 2 days ago',
+        status: 'Active',
+        image: draftImage,
+        viewButton: true,
+        editButton: true,
+    });
+
+
+    return (
+        <div>
+            <div className="max-w-7xl mx-auto">
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+                    {cards.map((card, index) => (
+                        <DashboardCard key={index} card={card} />
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default DraftsPage;
