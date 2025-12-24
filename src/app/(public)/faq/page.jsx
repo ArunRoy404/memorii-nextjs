8c45197abd4e34a38870a04d430fd62d907a81db
Arun Roy <arunroy0065@gmail.com>
2025-12-24 15:55:35 +0600

    feat: FAQPage created

---
+++ src/app/(public)/faq/page.jsx
@@ -0,0 +1,4 @@
+import FAQPage from '@/pages/landing/FAQPage';
+
+const faq = () => <FAQPage />
+export default faq;
--- src/components/HomePageComponents/FAQSection/FAQSection.jsx
+++ src/components/HomePageComponents/FAQSection/FAQSection.jsx
@@ -7,20 +7,28 @@ import {
 import CommonSection from "@/components/common/CommonSection/CommonSection";
 import ColorfulText from "@/components/ui/ColorfulText";
 import faqData from "@/data/faqData";
+import { cn } from "@/lib/utils";
 
-const FAQSection = () => {
+const FAQSection = ({ classNameContainer, limit }) => {
+
+
+    const limitedFaqData = faqData.slice(0, limit);
+
+
     return (
         <CommonSection
             title={<>Frequently Ask <ColorfulText>Questions</ColorfulText></>}
         >
 
-
             {/* faq accordion  */}
             <Accordion type="single" collapsible defaultValue={1}>
-                <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 lg:gap-10">
+                <div className={cn(
+                    "grid grid-cols-1 lg:grid-cols-2 gap-4 lg:gap-10",
+                    classNameContainer
+                )}>
                     <div className="justify-between flex flex-col item-start gap-4 lg:gap-7">
-                        {faqData?.map((faq, idx) => {
-                            if (idx < faqData.length / 2) {
+                        {limitedFaqData?.map((faq, idx) => {
+                            if (idx < limitedFaqData.length / 2) {
                                 return (
                                     <AccordionItem key={idx} value={idx + 1}>
                                         <AccordionTrigger>
@@ -33,8 +41,8 @@ const FAQSection = () => {
                         })}
                     </div>
                     <div className="justify-between flex flex-col item-start gap-4 lg:gap-7">
-                        {faqData?.map((faq, idx) => {
-                            if (idx >= faqData.length / 2) {
+                        {limitedFaqData?.map((faq, idx) => {
+                            if (idx >= limitedFaqData.length / 2) {
                                 return (
                                     <AccordionItem key={idx} value={idx + 1}>
                                         <AccordionTrigger>
