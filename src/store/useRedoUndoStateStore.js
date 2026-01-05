c5bfa5dc1ea9caa841fde66afab75f498bd2d10e
Arun Roy <arunroy0065@gmail.com>
2026-01-05 14:21:37 +0600

    feat: redo undo store created

---
+++ src/store/useRedoUndoStateStore.js
@@ -0,0 +1,28 @@
+import { create } from "zustand";
+
+export const useRedoUndoStateStore = create((set, get) => ({
+    allHistory: {},
+    allRedoStack: {},
+
+    setHistoryAt: (pageIndex, history) => {
+        set((state) => ({
+            allHistory: { ...state.allHistory, [pageIndex]: history },
+        }));
+    },
+
+    setRedoStackAt: (pageIndex, redoStack) => {
+        set((state) => ({
+            allRedoStack: { ...state.allRedoStack, [pageIndex]: redoStack },
+        }));
+    },
+
+
+    getPageState: (pageIndex) => {
+        const history = get().allHistory[pageIndex] || [];
+        const redoStack = get().allRedoStack[pageIndex] || [];
+        return { history, redoStack };
+    },
+
+    resetRedoUndoStateStore: () => set({ allHistory: {}, allRedoStack: {} }),
+}));
+
