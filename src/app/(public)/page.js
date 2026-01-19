d101a82b26d1aec0b3cf9e5d13bf589c85eeffae
Arun Roy <arunroy0065@gmail.com>
2026-01-19 16:45:12 +0600

    feat: Hero Section API implemented

--- src/app/(public)/page.js
+++ src/app/(public)/page.js
@@ -1,4 +1,35 @@
+import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query';
 import HomePage from '@/pages/landing/HomePage';
+import { getDynamicSections } from '@/hooks/cms.hook';
 
-const Home = () => <HomePage />
+
+export async function generateMetadata() {
+    const sections = await getDynamicSections();
+    const hero = sections?.find(s => s.section === 'hero_section');
+
+    return {
+        title: hero?.title,
+        description: hero?.short_description,
+        openGraph: {
+            images: [hero?.photo],
+        },
+    };
+}
+
+
+const Home = async () => {
+    const queryClient = new QueryClient();
+
+    await queryClient.prefetchQuery({
+        queryKey: ['dynamic-sections'],
+        queryFn: getDynamicSections,
+    });
+
+    return (
+        <HydrationBoundary state={dehydrate(queryClient)}>
+            <HomePage />
+        </HydrationBoundary>
+    );
+};
+
 export default Home;
