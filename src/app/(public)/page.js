9cd8459b263d5f5e46926fe5dc5ce1f434805092
Arun Roy <arunroy0065@gmail.com>
2026-01-19 16:48:45 +0600

    refactor: metadata removed

--- src/app/(public)/page.js
+++ src/app/(public)/page.js
@@ -3,19 +3,6 @@ import HomePage from '@/pages/landing/HomePage';
 import { getDynamicSections } from '@/hooks/cms.hook';
 
 
-export async function generateMetadata() {
-    const sections = await getDynamicSections();
-    const hero = sections?.find(s => s.section === 'hero_section');
-
-    return {
-        title: hero?.title,
-        description: hero?.short_description,
-        openGraph: {
-            images: [hero?.photo],
-        },
-    };
-}
-
 
 const Home = async () => {
     const queryClient = new QueryClient();
