8564e55e7de8bd11149ab681e0e819e83fd5ec2a
Arun Roy <arunroy0065@gmail.com>
2026-01-20 11:49:53 +0600

    feat: terms and condition API implemented

--- src/components/TermsComponents/TermsList.jsx
+++ src/components/TermsComponents/TermsList.jsx
@@ -4,7 +4,6 @@ import {
     AccordionItem,
     AccordionTrigger,
 } from "@/components/ui/accordion"
-import termSections from '@/data/termsSectionData';
 
 
 const TermsList = ({ termsConditions }) => {
