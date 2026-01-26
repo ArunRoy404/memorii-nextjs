3de3377e218ccb84653ce343a7b18c6cea870445
Arun Roy <arunroy0065@gmail.com>
2026-01-26 13:01:01 +0600

    feat: template page pagination added

--- src/components/HomePageComponents/ChooseTemplateSection/ChooseTemplateSection.jsx
+++ src/components/HomePageComponents/ChooseTemplateSection/ChooseTemplateSection.jsx
@@ -2,14 +2,15 @@
 
 import CommonSection from '@/components/common/CommonSection/CommonSection';
 import { Button } from '@/components/ui/button';
-import templateData from '@/data/templateData';
 import Link from 'next/link';
 import React from 'react';
 import TemplateCard from './TemplateCard';
 import { useGetSections } from '@/hooks/cms.hook';
+import { useGetTemplates } from '@/hooks/templates.hook';
 
 const ChooseTemplateSection = () => {
     const { data: sections } = useGetSections()
+    const { data: templates } = useGetTemplates()
     const sectionData = sections?.find(section => section?.section === 'template_occasions')
 
     return (
@@ -19,7 +20,7 @@ const ChooseTemplateSection = () => {
         >
 
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10'>
-                {templateData.slice(0, 4)?.map((template) => (
+                {templates?.slice(0, 4)?.map((template) => (
                     <TemplateCard
                         template={template}
                         key={template?.id}
