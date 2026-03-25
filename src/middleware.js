096596c7176f2040a4e8ea4585c0e223b2bf7288
unknown <arunroy0065@gmail.com>
2026-03-25 17:25:03 +0600

    feat: redirect url fixed for middleware

--- src/middleware.js
+++ src/middleware.js
@@ -37,14 +37,10 @@ export async function middleware(req) {
   // 2. If user is not logged in and trying to access a protected route (dashboard), redirect to login
   if (isProtectedRoute) {
     if (!isLoggedIn) {
-      let callbackUrl = nextUrl.pathname;
-      if (nextUrl.search) {
-        callbackUrl += nextUrl.search;
-      }
-
-      const encodedCallbackUrl = encodeURIComponent(callbackUrl);
+      const nextPath = nextUrl.pathname + nextUrl.search;
+      const encodedNext = encodeURIComponent(nextPath);
       return NextResponse.redirect(
-        new URL(`${DEFAULT_LOGOUT_REDIRECT}?callbackUrl=${encodedCallbackUrl}`, nextUrl)
+        new URL(`${DEFAULT_LOGOUT_REDIRECT}?next=${encodedNext}`, nextUrl)
       );
     }
   }
